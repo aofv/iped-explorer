b4a93aea50a399da260874a210843fdb3f9a92c0
Griffin Mahoney <gmahoney@athletesofvalor.com>
2017-12-20 13:56:43 -0500

    Gets rid of boilerplate code

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Schools from '@/components/schools/Index'
 
 Vue.use(Router)
 
+const routes = [
+  { path: '/schools', name: 'Schools', component: Schools },
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: routes,
+  mode: 'history',
+  linkExactActiveClass: 'is-active',
 })
